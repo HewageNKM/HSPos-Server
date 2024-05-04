dd184d25dda07679cbd56f2b1cd2820a28d0818a
Nadun Kawishika <hnkmalwenna@gmail.com>
2024-05-04 21:58:43 +0530

    add buildspec.yml file

--- src/main/java/com/nadunkawishika/helloshoesapplicationserver/application/Application.java
+++ src/main/java/com/nadunkawishika/helloshoesapplicationserver/application/Application.java
@@ -84,7 +84,7 @@ public class Application {
                 new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
+        config.addAllowedOriginPattern("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
