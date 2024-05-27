8134ad67814e3d66e36168ebbd93b5be6b1c48ff
Nadun Kawishika <hnkmalwenna@gmail.com>
2024-05-27 14:48:55 +0530

    All the Search result are limit to 20 raws

--- src/main/java/com/nadunkawishika/helloshoesapplicationserver/api/auth/User.java
+++ src/main/java/com/nadunkawishika/helloshoesapplicationserver/api/auth/User.java
@@ -3,10 +3,12 @@ package com.nadunkawishika.helloshoesapplicationserver.api.auth;
 import com.nadunkawishika.helloshoesapplicationserver.dto.resAndReq.LoginRequest;
 import com.nadunkawishika.helloshoesapplicationserver.dto.resAndReq.LoginResponse;
 import com.nadunkawishika.helloshoesapplicationserver.dto.resAndReq.RegisterRequest;
+import com.nadunkawishika.helloshoesapplicationserver.service.MailService;
 import com.nadunkawishika.helloshoesapplicationserver.service.UserService;
 import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 public class User {
     private final UserService userService;
     private final Logger LOGGER = org.slf4j.LoggerFactory.getLogger(User.class);
+    private final MailService mailService;
 
     @PostMapping("/login")
     public LoginResponse authenticate(@Validated @RequestBody LoginRequest loginRequest) {
@@ -41,4 +44,13 @@ public class User {
         userService.updatePassword(registerRequest);
     }
 
+    @GetMapping("/otp/{email}")
+    public void sendMail(@PathVariable @Pattern(regexp = "^[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}$") String email) {
+        mailService.sendOTP(email);
+    }
+
+    @GetMapping("/otp/verify/{otp}")
+    public ResponseEntity<Object> verifyMail(@PathVariable @Pattern(regexp = "^[0-9]{4}$") String otp) {
+        return mailService.verifyOTP(otp);
+    }
 }
